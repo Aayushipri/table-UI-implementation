79a821642715e7494c9e0d7a43ec567656386090
Aayushipri <39034084+Aayushipri@users.noreply.github.com>
2024-12-04 16:11:43 +0530

    implementing pagination changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { tableData } from "./tableData";
+
+const INITIAL_OFFSET = 0;
+const FINAL_OFFSET = 2;
+const TABLE_SIZE = 3;
 
 function App() {
+  const [initialOffset, setInitialOffset] = useState(INITIAL_OFFSET);
+  const [finalOffset, setFinalOffset] = useState(FINAL_OFFSET);
+
+  const updateOffset = (pageNum) => {
+    setInitialOffset(INITIAL_OFFSET + TABLE_SIZE * (pageNum - 1));
+    setFinalOffset(FINAL_OFFSET + TABLE_SIZE * (pageNum - 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="tableAlign">
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th> Profession</th>
+          </tr>
+          {tableData.map((data, index) => {
+            if (index >= initialOffset && index <= finalOffset) {
+              return (
+                <tr>
+                  <td>{data.id}</td>
+                  <td> {data.name}</td>
+                  <td>{data.profession}</td>
+                </tr>
+              );
+            }
+          })}
+        </table>
+      </div>
+      <div className="buttonStyle">
+        <button onClick={() => updateOffset(1)}>1</button>
+        <button onClick={() => updateOffset(2)}>2</button>
+        <button onClick={() => updateOffset(3)}>3</button>
+      </div>
     </div>
   );
 }
---
+++ src/tableData.js
@@ -0,0 +1,47 @@
+export const tableData = [
+  {
+    id: 1234,
+    name: "Ayushi Priya",
+    profession: "Senior Software Engineer",
+  },
+  {
+    id: 3456,
+    name: "Shovit",
+    profession: "Lawyer",
+  },
+  {
+    id: 789,
+    name: "Shraddha",
+    profession: "Human Resource Manager",
+  },
+  {
+    id: 1568,
+    name: "Priyanshu",
+    profession: "Software Engineer",
+  },
+  {
+    id: 9289,
+    name: "Suresh Prasad",
+    profession: "Inspector Of Police",
+  },
+  {
+    id: 8722,
+    name: "Aradhana Kumari",
+    profession: "Home Maker",
+  },
+  {
+    id: 7622,
+    name: "Vedika Badwaik",
+    profession: "Senior Software Engineer",
+  },
+  {
+    id: 8333,
+    name: "Amit Tulsyan",
+    profession: "Software Engineer",
+  },
+  {
+    id: 88282,
+    name: "Ruhi",
+    profession: "Manager",
+  },
+];
